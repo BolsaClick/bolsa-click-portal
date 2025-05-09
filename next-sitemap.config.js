fd58981b2c2ef282ac65afc0b7a86a5fb767bf6b
Rodrigo Silverio <rodrigo.silverio@compasso.com.br>
2025-05-09 13:08:10 -0300

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,13 +4,7 @@ module.exports = {
   sitemapSize: 5000,
   changefreq: 'weekly',
   priority: 0.7,
-  exclude: [
-    '/admin',
-    '/login',
-    '/checkout',
-    '/checkout/success',
-    '/buscar-cursos', 
-  ],
+  exclude: ['/admin', '/login', '/checkout', '/checkout/success'],
   robotsTxtOptions: {
     policies: [
       {
@@ -20,31 +14,5 @@ module.exports = {
     ],
   },
 
-  additionalPaths: async () => {
-    const res = await fetch('https://api.inovit.io/api/core/showCourse')
-    const courses = await res.json()
 
-    const paths = []
-
-    for (const course of courses) {
-      const slug = course.slug || course.name.toLowerCase().replace(/\s+/g, '-')
-      const modalidades = ['presencial', 'distancia', 'semipresencial']
-
-      for (const modalidade of modalidades) {
-        for (const unidade of course.units || []) {
-          const city = unidade.city.toLowerCase().replace(/\s+/g, '-')
-          const state = unidade.state.toLowerCase()
-          const cidadeEstado = `${city}-${state}`
-
-          paths.push({
-            loc: `/cursos/${slug}/${modalidade}/${cidadeEstado}`,
-            changefreq: 'weekly',
-            priority: 0.8,
-          })
-        }
-      }
-    }
-
-    return paths
-  },
 }

e5f5a905f5d9124e7712e13ca306e40701860b5b
Rodrigo Silverio <rodrigo.silverio@compasso.com.br>
2025-05-20 11:20:41 -0300

    fix seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,9 @@
 const coursePages = [
   {
     slug: 'direito',
-    city: 'São Paulo',
+    courseName:'Direito+-+Bacharelado',
+    citySlug: 'sao-paulo',
+    city: 'São+Paulo',
     state: 'SP',
     courseId: '01d29fc8-6e92-4a6e-8edc-848dab59da24',
     modalidade: 'presencial'
@@ -9,14 +11,18 @@ const coursePages = [
   {
     slug: 'psicologia',
     city: 'Campinas',
+    citySlug: 'campinas',
+    courseName:'Psicologia+-+Bacharelado',
     state: 'SP',
     courseId: '05ea2de4-cf8c-4b2d-a3eb-9ab3bb8b94cf',
     modalidade: 'presencial'
   },
   {
     slug: 'administracao',
-    city: 'Ribeirão Preto',
+    citySlug: 'ribeirao-preto',
+    city: 'Ribeirão+Preto',
     state: 'SP',
+    courseName:'Administração+-+Bacharelado',
     courseId: 'aaa8f97d-40dc-40c6-b772-2c3068288534',
     modalidade: 'distancia'
 
@@ -24,7 +30,9 @@ const coursePages = [
 
   {
     slug: 'enfermagem',
-    city: 'Rio de Janeiro',
+    city: 'Rio+de+Janeiro',
+    citySlug: 'rio-de-janeiro',
+    courseName:'Enfermagem+-+Bacharelado',
     state: 'RJ',
     courseId: '9fd0c921-93b8-400c-82c6-f1a0166402d7',
     modalidade: 'presencial'
@@ -32,6 +40,8 @@ const coursePages = [
   },
   {
     slug: 'educacao-fisica',
+    citySlug: 'curitiba',
+    courseName:'Educação+Física+-+Bacharelado',
     city: 'Curitiba',
     state: 'PR',
     courseId: '364d1436-ad27-4035-9ba7-dcb503797ed6',
@@ -40,7 +50,9 @@ const coursePages = [
   },
   {
     slug: 'farmacia',
-    city: 'Belo Horizonte',
+    citySlug: 'belo-horizonte',
+    city: 'Belo+Horizonte',
+    courseName:'Farmácia+-+Bacharelado',
     state: 'MG',
     courseId: 'd4f0d30d-75a3-4964-b5dc-028ae835460c',
     modalidade: 'distancia'
@@ -64,8 +76,8 @@ module.exports = {
     ],
   },
   additionalPaths: async () => {
-    const paths = coursePages.map(({ slug, city, state, courseId, modalidade }) => ({
-      loc: `/cursos/resultado/${modalidade}/${slug}/${city}?city=${city}&&state=${state}courseId=${courseId}&courseName=${encodeURIComponent(slug)}`,
+    const paths = coursePages.map(({ slug, citySlug, city, state, courseId, modalidade, courseName }) => ({
+      loc: `/cursos/resultado/${modalidade}/${slug}/${citySlug}?courseId=${courseId}&city=${city}&state=${state}&courseName=${courseName}`,
       changefreq: 'weekly',
       priority: 0.8,
     }))

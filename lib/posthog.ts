e31530cd846476abdd261796163b1edb513f9971
Rodrigo Silverio <rodrigo.silverio@compasso.com.br>
2025-05-31 10:19:37 -0300

    posthog

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -1,10 +1,13 @@
-import { PostHog } from "posthog-node"
+import posthog from 'posthog-js'
 
-export default function PostHogClient() {
-  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    flushAt: 1,
-    flushInterval: 0,
-  })
-  return posthogClient
+export function initPostHog() {
+  if (typeof window !== 'undefined' && !posthog.__loaded) {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+      autocapture: true,
+      capture_pageview: true,
+    })
+  }
 }
+
+export default posthog

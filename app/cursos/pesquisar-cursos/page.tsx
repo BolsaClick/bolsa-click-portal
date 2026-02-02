5fe1e8deb84b41292f3a3a429831a5226f8ed0ff
Rodrigo Silverio <rodrigosilverio@Rodrigos-MacBook-Air.local>
2026-02-02 18:47:58 -0300

    update seo

--- app/cursos/pesquisar-cursos/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function PesquisarCursosPage() {
-  // Redirecionar para a página de resultados de cursos
-  redirect('/curso/resultado')
-}
-
